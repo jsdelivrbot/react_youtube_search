d899087104c4b0ff8e371f0b3a5616a192854319
Kalo Pilato <kalo@Kalos-MacBook-Pro.local>
2017-04-04 13:28:14 +1200

    Initial commit - basic youtube video search implementation

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
+
+const API_KEY = 'AIzaSyCyYGPrd5lwq-doNrqfjnQyZQlBLmcz-I8';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('react js');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => this.videoSearch(term), 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
